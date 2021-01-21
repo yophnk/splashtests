7ea7c2eb62aba3774e7d2541de18fd35183b778f
me <me@doooooop.com>
2021-01-21 11:55:36 -0800

    Initial Commit

---
+++ splashsimple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splashsimple
+//
+//  Created by Brandon Foy on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
